87a7be2085e1c3753095b2a2c3a6a5902aaf4f33
你脸真肥 <1425080898@qq.com>
2020-06-08 18:34:17 +0800

    提交：1

---
+++ src/main/java/com/aaa/firstgit/FirstgitApplication.java
@@ -0,0 +1,14 @@
+package com.aaa.firstgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//测试git
+@SpringBootApplication
+public class FirstgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstgitApplication.class, args);
+    }
+
+}
